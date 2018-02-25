d69766470afac89e8e465b1cfc11db2738bd2348
trig0n <pascal@eberlein.io>
2018-02-25 22:15:34 +0100

    as class

--- esp8266.py
+++ esp8266.py
@@ -4,37 +4,52 @@ from solid.utils import *
 
 SEGMENTS = 42
 NAME = "esp8266"
-HAS_PINS = True
 
 
-def assemble():
-    base = cube([57.5, 31, 1.5])
+class ESP8266:
+    length = 57.5
+    width = 31
+    height = 1.5
 
-    _hole_cylinder = hole()(down(20)(cylinder(d=3, h=100)))
-    base += right(2)(forward(2)(_hole_cylinder))
-    base += right(2)(forward(29)(_hole_cylinder))
-    base += right(54.8)(forward(2)(_hole_cylinder))
-    base += right(54.8)(forward(29)(_hole_cylinder))
+    _hole_diameter = 3
 
-    _cube_port = cube([5.7, 7.4, 2.4])
-    base += forward(31 / 2 - 5.7 / 2)(left(1.5)(_cube_port))
+    _pin_height = 5.9
+    _pin_diameter = 0.6
+    _pin_base = cube([2.5, 2.5, 2.5])
 
-    _cube_btn = up(1.5)(cube([4, 3, 1.5]))
-    _cube_btn += up(2.5)(right(2)(forward(1.5)(cylinder(d=1.5, h=1))))
-    base += forward(6)(right(2)(_cube_btn))
-    base += forward(22)(right(2)(_cube_btn))
+    def __init__(self, has_pins=True):
+        self.has_pins = has_pins
 
-    _cube_chip = cube([24.2, 16, 1])
-    _cube_chip += forward(2)(right(1)(cube([15, 12, 1.2])))
-    base += forward(7.6)(right(33.2)(up(1.5)(_cube_chip)))
+    def assemble(self):
+        base = cube([self.length, self.width, self.height])
 
-    if HAS_PINS:
-        pin_base = cube([37.7, 2.5, 8.5])
-        base += down(8.5)(right(10)(pin_base))
-        base += down(8.5)(right(10)(forward(28.6)(pin_base)))
+        _hole_cylinder = hole()(down(20)(cylinder(d=self._hole_diameter, h=100)))
+        base += right(2)(forward(2)(_hole_cylinder))
+        base += right(2)(forward(29)(_hole_cylinder))
+        base += right(54.8)(forward(2)(_hole_cylinder))
+        base += right(54.8)(forward(29)(_hole_cylinder))
 
-    return base
+        _cube_port = cube([5.7, 7.4, 2.4])
+        base += forward(self.width / 2 - 5.7 / 2)(left(1.5)(_cube_port))
+
+        _cube_btn = up(1.5)(cube([4, 3, self.height]))
+        _cube_btn += up(2.5)(right(2)(forward(1.5)(cylinder(d=1.5, h=1))))
+        base += forward(6)(right(2)(_cube_btn))
+        base += forward(22)(right(2)(_cube_btn))
+
+        _cube_chip = cube([24.2, 16, 1])
+        _cube_chip += forward(2)(right(1)(cube([15, 12, 1.2])))
+        base += forward(7.6)(right(33.2)(up(1.5)(_cube_chip)))
+
+        if self.has_pins:
+            for i in range(0, 14):
+                base += right(10 + i * 2.5)(down(2.5)(self._pin_base))
+            pin_base = cube([37.7, 2.5, 8.5])
+            base += down(8.5)(right(10)(pin_base))
+            base += down(8.5)(right(10)(forward(28.6)(pin_base)))
+
+        return base
 
 
 if __name__ == '__main__':
-    scad_render_to_file(assemble(), join('./out/', "esp8266.scad"), file_header='$fn = %s;' % SEGMENTS)
+    scad_render_to_file(ESP8266().assemble(), join('./out/', NAME + ".scad"), file_header='$fn = %s;' % SEGMENTS)

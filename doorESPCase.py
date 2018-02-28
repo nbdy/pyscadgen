c56c8df32879537dbf21529771e162dd78ce55bb
trig0n <pascal@eberlein.io>
2018-02-28 19:51:49 +0100

    gon b useful

--- doorESPCase.py
+++ doorESPCase.py
@@ -15,7 +15,7 @@ class DoorESPCase:
     width = ESP8266.width
     height = ESP8266.height
 
-    def __init__(self, extra=8):
+    def __init__(self, extra=4):
         self.esp = ESP8266()
         self.length += extra
         self.width += extra
@@ -26,6 +26,7 @@ class DoorESPCase:
         case = cube([self.length, self.width, self.height])
         case += right(self.extra / 2)(
             up(self.height / 2 - self.esp.height / 2)(forward(self.extra / 2)(hole()(ESP8266().assemble()))))
+        case += hole()(cube([self.extra / 4, self.width, self.height]))
         return case
 
     def upper_half(self):
@@ -40,4 +41,4 @@ class DoorESPCase:
 
 
 if __name__ == '__main__':
-    scad_render_to_file(DoorESPCase().lower_half(), join('./out/', NAME + ".scad"), file_header='$fn = %s;' % SEGMENTS)
+    scad_render_to_file(DoorESPCase().upper_half(), join('./out/', NAME + ".scad"), file_header='$fn = %s;' % SEGMENTS)
